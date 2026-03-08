a389e18f42d153f7188b3123189bbc97f87e6a0d
Anna Yudkovsky <anya.yar@gmail.com>
2026-03-08 13:54:33 -0400

    Initial Commit

---
+++ DualWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DualWeather
+//
+//  Created by Anya Yudkovsky on 3/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DualWeather/DualWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualWeatherApp.swift
+//  DualWeather
+//
+//  Created by Anya Yudkovsky on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct DualWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
